d8feeeae2236a65e3e7f286641d58b066e6103eb
SatoIshii <ishii-sato@webrage.jp>
2021-08-18 17:00:03 +0900

    [update] 確認ダイアログ、エラー文追加
    未入力がなければDBへ新規登録を行う機能を追加しました。

--- include/check.js
+++ include/check.js
@@ -1,9 +1,11 @@
+//入力フォームに対してのバリデーションチェックをするファイル
 function check(){
     var name_flag=0;
     var pref_flag=0;
     var age_flag=0;
     var age_value = document.entryform.age.value;
-    
+
+    //入力判定
     if(document.entryform.namae.value==""){
         name_flag=1;
     }else if(document.entryform.pref.value=="0"){
@@ -12,19 +14,26 @@ function check(){
         age_flag=1;
     }
 
+    //エラー文表示
     if(name_flag==1){
         window.alert('名前は必須です');
         return false;
-    }else if(pref_flag==1){
+    }
+
+    if(pref_flag==1){
         window.alert('都道府県は必須です');
         return false;
-    }else if(age_flag==1 || age_value < 1 || age_value > 100){
+    }
+
+    if(age_flag==1 || age_value < 1 || age_value > 100){
         window.alert('年齢は必須です/数値を入力してください/1-99の範囲で入力してください');
         return false;
-    }else if(window.confirm('更新を行います。よろしいですか？')){
-            location.href="../system/index.php";//「OK」の場合はindex.phpに移動
-    }else{
-            windows.alert('キャンセルされました');//警告ダイアログ
-            return false;//送信を中止
     }
+
+    if(window.confirm('更新を行います。よろしいですか？')){
+        document.entryform.submit();//「OK」の場合はindex.phpに移動
+    }//else{
+        //windows.alert('キャンセルされました');//警告ダイアログ
+        //return false;//送信を中止
+    //}
 }
