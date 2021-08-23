9e8e18e0f875d77330b54372093f86937a8d4e3a
SatoIshii <ishii-sato@webrage.jp>
2021-08-23 14:54:06 +0900

    [add] delite.php追加　
    中身はありません。
    これから作成します。
    
    [update]　detail01.php機能完成
    
    top画面から詳細に飛べるリンクを追加しました。

--- system/detail01.php
+++ system/detail01.php
@@ -17,47 +17,35 @@
     </script>
     <body>
         <?php
-                include("../include/staticdatas.php");
-                include("../include/DB_access.php");
-                include("../include/header.html");
-                ?>
-                <?php
-                    $DB_DSN = "mysql:host=localhost; dbname=sishii; charset=utf8";
-                    $DB_USER = "webaccess";
-                    $DB_PW = "toMeu4rH";
-                    $pdo = new PDO($DB_DSN, $DB_USER, $DB_PW);
-                ?>
-        <table border="3" style="width:50%""border-collapse:collapse "border="0">
-            <tr>
-                <th style="width:30%"bgcolor="#add8e6">社員ID</th>
-                <td></td>
-            </tr>
-            <tr>
-                <th style="width:30%" bgcolor="#add8e6">名前</th>
-                <td></td>
-            </tr>
-            <tr>
-                <th style="width:30%"bgcolor="#add8e6">出身地</th>
-                <td></td>
-            </tr>
-            <tr>
-                <th style="width:30%"bgcolor="#add8e6">性別</th>
-                <td></td>
-            </tr>
-            <tr>
-                <th style="width:30%"bgcolor="#add8e6">年齢</th>
-                <td></td>
-            </tr>
-            <tr>
-                <th style="width:30%"bgcolor="#add8e6">所属部署</th>
-                <td></td>
-            </tr>
-            <tr>
-                <th style="width:30%"bgcolor="#add8e6">役職</th>
-                <td></td>
-        </table>
-        </div>
-            <a href="./entry_update01.php"> <input type=submit value=" 編集 "></a>
+            //ファイル読み込み
+            include("../include/staticdatas.php");
+            include("../include/DB_access.php");
+            include("../include/header.html");
+        ?>
+        <?php
+            //index.phpから受け取ったidを変数に入れる
+            $id = $_GET['member_ID'];
+
+            //名前検索で詳細を表示
+            $query_str_02 = "SELECT member.member_ID, member.name, member.pref, member.seibetu, member.age, grade_master.grade_name, section1_master.section_name
+                             FROM member LEFT JOIN grade_master ON grade_master.ID = member.grade_ID
+                             LEFT JOIN section1_master ON section1_master.ID = member.section_ID
+                             WHERE member_ID = '$id'";
+            #echo $query_str_02;
+            $sql = $pdo->prepare($query_str_02);                // PDOオブジェクトにSQLを渡す
+            $sql->execute();                                    // SQLを実行する
+            $result = $sql->fetchAll();
+
+            foreach($result as $each){
+              $pref = $each['pref'];
+              $gender = $each['seibetu'];
+          }
+          //出力ファイル読み込み
+            include("../include/output.php");
+
+        ?>
+
+            <a href="./entry_update01.php?member_ID=<?php echo $id;?>"><input type=submit value="編集"></a>
             <input type="button" value="削除" onclick="goDel(ID);">
     </body>
 </html>
