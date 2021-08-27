861368adf1cda208e23ad07e9e70c23271bc7f9c
SatoIshii <ishii-sato@webrage.jp>
2021-08-27 14:22:21 +0900

    [update] URL直打ち対策
    member_IDが取得ができなかった時、
    id検索でDBに情報が無かった時にエラーページに遷移するように変更しました。

--- system/delete01.php
+++ system/delete01.php
@@ -1,14 +1,12 @@
 <?php
     include("../include/DB_access.php");
-    
-    /*URLに載せて渡されたidはGETで取得できる*/
     $id = $_GET['member_ID'];
-
     $delete_str="DELETE FROM member WHERE member_ID='$id'";
-    /*SQL文実行*/
+    //本番ではコメントアウト外す
+
     $sql = $pdo->prepare($delete_str);
     $sql->execute();
-
+    
     #echo $delete_str;
     //トップ画面へ遷移
     header('Location:index.php');
--- system/detail01.php
+++ system/detail01.php
@@ -23,15 +23,36 @@
         ?>
         <?php
             //index.phpから受け取ったidを変数に入れる
-            $id = $_GET['member_ID'];
 
-            //名前検索で詳細を表示
-            $query_str_02 = "SELECT member.member_ID, member.name, member.pref, member.seibetu, member.age, grade_master.grade_name, section1_master.section_name
-                             FROM member LEFT JOIN grade_master ON grade_master.ID = member.grade_ID
-                             LEFT JOIN section1_master ON section1_master.ID = member.section_ID
-                             WHERE member_ID = '$id'";
-            #echo $query_str_02;
+            if(isset($_GET['member_ID']) && !empty($_GET['member_ID'])){
+                $id = $_GET['member_ID'];
+                //名前検索で詳細を表示
+                $query_str_02 = "SELECT member.member_ID, member.name, member.pref, member.seibetu, member.age, grade_master.grade_name, section1_master.section_name
+                                 FROM member LEFT JOIN grade_master ON grade_master.ID = member.grade_ID
+                                 LEFT JOIN section1_master ON section1_master.ID = member.section_ID
+                                 WHERE member_ID = '$id'";
+                #echo $query_str_02;
 
+                $sql = $pdo->prepare($query_str_02);                // PDOオブジェクトにSQLを渡す
+                $sql->execute();                                    // SQLを実行する
+                $result = $sql->fetchAll();
+
+                if(isset($result) && !empty($result)){
+                    foreach($result as $each){
+                      $pref = $each['pref'];
+                      $gender = $each['seibetu'];?>
+                      <div class="output" id="tbl-bdr">
+                          <?php include("../include/output.php");?>
+                      </div><?php
+                    }
+                }else{
+                    header('Location:../include/error.php');
+                }
+          }else{
+              header('Location:../include/error.php');
+          }
+
+/*
             $sql = $pdo->prepare($query_str_02);                // PDOオブジェクトにSQLを渡す
             $sql->execute();                                    // SQLを実行する
             $result = $sql->fetchAll();
@@ -40,16 +61,8 @@
               $pref = $each['pref'];
               $gender = $each['seibetu'];
           }
-
+*/
         ?>
-        <div class="output" id="tbl-bdr">
-            <?php
-            //出力ファイル読み込み
-            include("../include/output.php");
-            //header('Location:../incldue/error.php);
-            ?>
-        </div>
-
         <div class="btn">
             <a href="./entry_update01.php?member_ID=<?php echo $id;?>"><input class="btn_style" type=submit value="編集"></a>
             <input class="btn_style" type="button" value="削除" onclick="goDel(<?php echo $id;?>)">
