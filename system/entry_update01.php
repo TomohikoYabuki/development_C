f067db69971fc8197720f257c4d7412e4148722a
tomoya <wada-tomoya@webrage.jp>
2021-08-30 13:03:47 +0900

    [add]
    entry_update01.phpの修正03

--- system/entry_update01.php
+++ system/entry_update01.php
@@ -21,23 +21,42 @@
             include("../include/staticdatas.php");
             include("../include/DB_access.php");
             include("../include/header.html");
+            //var_dump($section_array);
+            $section_sql = "SELECT * FROM section1_master";
+
+            $sql = $pdo->prepare($section_sql);
+            $sql->execute();
+            $section_array = $sql->fetchAll();
+
+            $grade_sql = "SELECT * FROM grade_master";
+
+            $sql = $pdo->prepare($grade_sql);
+            $sql->execute();
+            $grade_array = $sql->fetchAll();
         ?>
         <?php
             $id = $_GET['member_ID'];
             //名前検索で詳細を表示
-            $query_str = "SELECT * FROM member WHERE member_ID = '$id'";
+            $query_str = "SELECT * FROM member WHERE member_ID = '$id'"; //変数の文字列展開修正する
             #echo $query_str_02;
         try{
             $sql = $pdo->prepare($query_str);                // PDOオブジェクトにSQLを渡す
             $sql->execute();                                    // SQLを実行する
             $result = $sql->fetchAll();
-                if(empty($result){
+                if(empty($result)){
                     header('Location:error.php');
                 }
+
         }catch(PDOException $e){
                 header('Location:error.php');
             }
+
         ?>
+        <pre>
+        <?php
+        //var_dump($result);
+        ?>
+        </pre>
         <form method='post' action='update02.php' name="mainform">
             <div class="output" id="tbl-bdr">
                 <table border="1" style="border-collapse:collapse;">
@@ -48,7 +67,7 @@
                     <!--名前-->
                     <tr>
                         <th>名前</th>
-                        <td><input type="text" name="namae" size="30" value="<?php echo $name;?>"></td>
+                        <td><input type="text" name="namae" size="30" value="<?php echo $result[0]["name"];?>"></td>
                     </tr>
                     <!--出身地-->
                     <tr>
@@ -56,11 +75,11 @@
                         <td>
                             <select name="pref">
                                 <!--初期値設定-->
-                                <option hidden value="<?php echo $pref;?>"><?php echo $pref_array[$pref];?></option>
+                                <option hidden value="<?php echo $pref;?>"><?php echo $pref_array[$result[0]["pref"]];?></option>
                                 <?php
-                                    for($i=1; $i<=47; $i++){
-                                        echo "<option value=". $i. ">" . $pref_array[$i] . "</option>";
-                                    }
+                                foreach($pref_array as $key => $value){
+                                    echo "<option value=". $key .">" . $value . "</option>";
+                                }
                                 ?>
                             </select>
                         </td>
@@ -69,34 +88,50 @@
                     <tr>
                         <th>性別</th>
                         <td>
-                            <!--$genderが1だったら"男"に"checked"を出力する方法で初期値設定-->
-                            <label><input type="radio" name="gender" value="1"<?php if($gender==1){echo "checked";}?>>男</label>
-                            <label><input type="radio" name="gender" value="2"<?php if($gender==2){echo "checked";}?>>女</label>
+                            <?php
+                            foreach($gender_array as $key => $value){
+                                echo "<label><input type='radio' name='gender' value='". $key ."'";
+                                if($key==$result[0]["seibetu"]){
+                                    echo " checked";
+                                }
+                                echo ">" . $value . "</label>";
+                            }
+                            ?>
                         </td>
                     </tr>
                     <!--年齢-->
                     <tr>
                         <th>年齢</th>
-                            <td><input type="number" name="age" value="<?php echo $age;?>" max="99" min="1" required> 才</td>
+                            <td><input type="number" name="age" value="<?php echo $result[0]["age"];?>" max="99" min="1" required> 才</td>
                     </tr>
                     <!--所属部署-->
                     <tr>
                         <th>所属部署</th>
-                        <td><input type='radio' id='sec1' name='section' value='1'<?php if($section==1){echo "checked";}?>><label for='sec1'>第一事業部</label>
-                            <input type='radio' id='sec2' name='section' value='2'<?php if($section==2){echo "checked";}?>><label for='sec2'>第二事業部</label>
-                            <input type='radio' id='sec3' name='section' value='3'<?php if($section==3){echo "checked";}?>><label for='sec3'>営業</label>
-                            <input type='radio' id='sec4' name='section' value='4'<?php if($section==4){echo "checked";}?>><label for='sec4'>総務</label>
-                            <input type='radio' id='sec5' name='section' value='5'<?php if($section==5){echo "checked";}?>><label for='sec5'>人事</label>
+                        <td>
+                            <?php
+                                foreach ($section_array as $each){
+                                    echo "<label><input type='radio' name='section' value='". $each['ID'] ."'";
+                                    if($each['ID']==$result[0]["section_ID"]){
+                                        echo " checked";
+                                    }
+                                    echo ">" . $each['section_name'] . "</label>";
+                                }
+                            ?>
                         </td>
                     </tr>
                     <!--役職-->
                     <tr>
                         <th>役職</th>
-                        <td><input type='radio' id='grd1' name='grade' value='1'<?php if($grade==1){echo "checked";}?>><label for='grd1'>事業部長</label>
-                            <input type='radio' id='grd2' name='grade' value='2'<?php if($grade==2){echo "checked";}?>><label for='grd2'>部長</label>
-                            <input type='radio' id='grd3' name='grade' value='3'<?php if($grade==3){echo "checked";}?>><label for='grd3'>チームリーダー</label>
-                            <input type='radio' id='grd4' name='grade' value='4'<?php if($grade==4){echo "checked";}?>><label for='grd4'>リーダー</label>
-                            <input type='radio' id='grd5' name='grade' value='5'<?php if($grade==5){echo "checked";}?>><label for='grd5'>メンバー</label>
+                        <td>
+                            <?php
+                                foreach ($grade_array as $each){
+                                    echo "<label><input type='radio' name='grade' value='". $each['ID'] ."'";
+                                    if($each['ID']==$result[0]["grade_ID"]){
+                                        echo " checked";
+                                    }
+                                    echo ">" . $each['grade_name'] . "</label>";
+                                }
+                            ?>
                         </td>
                     </tr>
 
