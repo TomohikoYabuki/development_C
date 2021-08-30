75dc0702f420b0cf5290a28e7dde91ca74ff232c
ishiisato <ishii-sato@webrage.jp>
2021-08-30 11:20:48 +0900

    [update] 細かいところいじりました

--- include/output.php
+++ include/output.php
@@ -2,7 +2,8 @@
     include("../include/staticdatas.php");
     echo "<table>";
 
-    foreach($result as $each){
+    /*foreach($result as $each){*/
+    //var_dump($each);
       echo "<tr><th>社員ID</th><td>" . $each['member_ID'] . "</td></tr>";
       echo "<tr><th>名前</th><td>" . $each['name'] . "</td></tr>";
       echo "<tr><th>出身地</th><td>" . $pref_array[$pref] . "</td></tr>";
@@ -10,6 +11,6 @@
       echo "<tr><th>年齢</th><td>" . $each['age'] . "</td></tr>";
       echo "<tr><th>所属部署</th><td>" . $each['section_name'] . "</td></tr>";
       echo "<tr><th>役職</th><td>" . $each['grade_name'] . "</td></tr>";
-    }
-    echo "</table></div>";
+    //}
+      echo "</table></div>";
 ?>
